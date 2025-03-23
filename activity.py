0ef7202cced942736f28d3a0bb6a379435444e9a
Wiktor Szyszka <wiktorszyszka.priv@gmail.com>
2025-03-23 19:36:10 +0100

    v3.1.1 - Gantt chart

--- activity.py
+++ activity.py
@@ -3,8 +3,51 @@ class Activity:
         self.name = name
         self.duration = duration
         self.predecessors = predecessors if predecessors else []
-        self.ES = 0  # Early Start
-        self.EF = 0  # Early Finish
-        self.LS = 0  # Late Start
-        self.LF = 0  # Late Finish
+        self.ES = 0
+        self.EF = 0
+        self.LS = 0
+        self.LF = 0
         self.reserve = 0
+
+
+def parsePredecessorformat(predecessor_data):
+    """
+    Input: Dictionary with activity names, durations, and predecessors.
+    Output: Dictionary of Activity objects.
+    """
+    activities = {}
+    for name, info in predecessor_data.items():
+        activities[name] = Activity(name, info['duration'], info.get('predecessors', []))
+    return activities
+
+
+def parseEventSequenceFormat(event_data):
+    """
+    Input: Dictionary with activity names, durations, and event sequences (e.g., '1-2').
+    Output: Dictionary of Activity objects with computed predecessors.
+    """
+
+    event_starts = {}
+    event_ends = {}
+    activities_temp = {}
+
+    for name, info in event_data.items():
+        duration = info['duration']
+        start_event, end_event = map(int, info['events'].split('-'))
+
+        activities_temp[name] = {'duration': duration, 'start': start_event, 'end': end_event}
+
+        if start_event not in event_starts:
+            event_starts[start_event] = []
+        if end_event not in event_ends:
+            event_ends[end_event] = []
+        event_starts[start_event].append(name)
+        event_ends[end_event].append(name)
+
+    activities = {}
+    for name, info in activities_temp.items():
+        start_event = info['start']
+        predecessors = event_ends.get(start_event, [])
+        activities[name] = Activity(name, info['duration'], predecessors)
+
+    return activities
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from activity import Activity
+from activity import Activity, parseEventSequenceFormat, parsePredecessorformat
 from cpm import CPM
 
 
 def main():
+    # Test data #
+
     # data = {
     #     'A': Activity('A', 2),
     #     'B': Activity('B', 5),
@@ -12,18 +14,18 @@ def main():
     #     'F': Activity('F', 2, ['D']),
     # }
 
-    data = {
-        'A': Activity('A', 3),
-        'B': Activity('B', 4,['A']),
-        'C': Activity('C', 6, ['A']),
-        'D': Activity('D', 7, ['B']),
-        'E': Activity('E', 1, ['D']),
-        'F': Activity('F', 2, ['C']),
-        'G': Activity('G', 3, ['C']),
-        'H': Activity('H', 4, ['G']),
-        'I': Activity('I', 1, ['E', 'F', 'H']),
-        'J': Activity('J', 2, ['I']),
-    }
+    # data = {
+    #     'A': Activity('A', 3),
+    #     'B': Activity('B', 4,['A']),
+    #     'C': Activity('C', 6, ['A']),
+    #     'D': Activity('D', 7, ['B']),
+    #     'E': Activity('E', 1, ['D']),
+    #     'F': Activity('F', 2, ['C']),
+    #     'G': Activity('G', 3, ['C']),
+    #     'H': Activity('H', 4, ['G']),
+    #     'I': Activity('I', 1, ['E', 'F', 'H']),
+    #     'J': Activity('J', 2, ['I']),
+    # }
 
     # data = {
     #     'A': Activity('A', 3),
@@ -39,12 +41,43 @@ def main():
     #     'K': Activity('K', 5, ['B']),
     # }
 
+    ###################
+
+    # Zad 1 #
+
+    data = {
+        'A': {'duration': 2, 'events': '1-2'},
+        'B': {'duration': 4, 'events': '2-3'},
+        'C': {'duration': 3, 'events': '1-3'},
+        'D': {'duration': 5, 'events': '3-4'},
+        'E': {'duration': 2, 'events': '2-5'},
+        'F': {'duration': 3, 'events': '4-6'},
+        'G': {'duration': 4, 'events': '5-6'},
+        'H': {'duration': 2, 'events': '6-7'},
+    }
+
+    data = parseEventSequenceFormat(data)
+
+    # Zad 2 #
+
+    # data = {
+    #     'A': Activity('A', 3),
+    #     'B': Activity('B', 5),
+    #     'C': Activity('C', 2, ['A']),
+    #     'D': Activity('D', 4, ['B']),
+    #     'E': Activity('E', 3, ['B']),
+    #     'F': Activity('F', 2, ['C', 'D']),
+    #     'G': Activity('G', 4, ['E']),
+    #     'H': Activity('H', 3, ['F', 'G']),
+    #     'I': Activity('I', 2, ['H']),
+    # }
+
     results = CPM(data)
     results.calculate()
     results.print()
     results.printCriticalPath()
-    # results.drawAON()
-    # results.drawAOA()
+    results.drawAON()
+    results.drawAOA()
     results.drawGantt()
 
 
