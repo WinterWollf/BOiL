5a1cba048d14ebbd4ceb913ed9fe6437e222770c
Paweł Socała <psocala12@gmail.com>
2025-03-28 19:31:08 +0100

    cpm window 1.0

--- gui_paths.py
+++ gui_paths.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path("assets/frame0")
+MAIN_ASSETS_PATH = OUTPUT_PATH / Path("assets/frame0")
+CPM_ASSETS_PATH = OUTPUT_PATH / Path("assets/frame1")
 FONTS_PATH = OUTPUT_PATH / Path("assets/fonts")
 
+
 def relative_to_assets(path: str) -> Path:
-    return ASSETS_PATH / Path(path)
+    return MAIN_ASSETS_PATH / Path(path)
+
+def relative_to_assets_2(path: str) -> Path:
+    return CPM_ASSETS_PATH / Path(path)
 
 def relative_to_fonts(path: str) -> Path:
     return FONTS_PATH / Path(path)
